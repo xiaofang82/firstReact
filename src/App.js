aa6a8f72896054344ec1d8b1ba5ac22295ef0aa5
XiaoFang <yangxiaofang82@gmail.com>
2024-05-29 14:23:59 -0500

    fix

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+// App is the main component
+/*
+Browsers can't 'read' JSX 
+convensions
+  - all element must be closed
+  - Empty elements (br, hr, img, input...) must be closed with a slash(/)
+  - JSX uses camelCase style(onClick, tabIndex...)
+  - Use curly braces to include literal JavaScript
+  - Use double curly braces with objects
+*/
+// spoiler: this is a hook 
+import { useState } from 'react';
 
 function App() {
+  const [userName,setUserName] = useState('');
+  console.log(userName);
+  let message = <h2>Welcome to React,<span>{userName}</span></h2>;
+  let isEyeClick = true;
+  const handleInput = (event) => {
+    setUserName(event.target.value);
+  }
+  //Here is okay
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <main>
+      <div>
+        {/** Ugly JSX comment */}
+        {message}
+        
+        <input 
+          type = {isEyeClick ? 'text' : 'password'}
+          placeholder='User Name'
+          onChange={handleInput}
+        />
+      </div>
+    </main>
+
   );
 }
 
